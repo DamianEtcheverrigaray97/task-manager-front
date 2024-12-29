e34de08f5a480040b8c5fcb5c8062865773a46f9
Damian Etcheverrigaray <damian.etcheverrigar@estudiantes.utec.edu.uy>
2024-12-29 13:47:04 -0300

    Environments: local and prod.

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const fetchTasks = async (completed?: boolean) => {
   let url = API_URL + 'tasks';
